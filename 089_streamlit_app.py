b438a027224e30f91930d9775660d44e197b5fbe
Khoirunisa Jannatuzzahra <105572998+KhoirunisaJ@users.noreply.github.com>
2024-05-07 13:00:34 +0700

    Update 089_streamlit_app.py

--- 089_streamlit_app.py
+++ 089_streamlit_app.py
@@ -1,16 +1,15 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # reading the database
 data = pd.read_csv("tips.csv")
 
 # Bar chart with day against tip
-fig, ax = plt.subplots()
-ax.bar(data['day'], data['tip'])
-ax.set_title("Bar Chart")
-ax.set_xlabel('Day')
-ax.set_ylabel('Tip')
+st.bar_chart(data.groupby('day')['tip'].mean())
 
-# Show plot in Streamlit app
-st.pyplot(fig)
+# Title for the chart
+st.title("Bar Chart")
+
+# Setting the X and Y labels
+st.xlabel('Day')
+st.ylabel('Tip')
