ab02e762b9fab6a1d256e7785acbdfe72e267f67
Khoirunisa Jannatuzzahra <105572998+KhoirunisaJ@users.noreply.github.com>
2024-05-07 13:27:07 +0700

    Update 089_streamlit_app.py

--- 089_streamlit_app.py
+++ 089_streamlit_app.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("tips.csv")
 
 st.title("My Dashboard")
+# Title for the chart
+st.title("Bar Chart")
 
 # Bagian visualisasi data
 st.header("Selamat Datang")
@@ -14,10 +16,16 @@ st.subheader("Jumlah Tips per hari")
 # Bar chart with day against tip
 st.bar_chart(data.groupby('day')['tip'].mean())
 
-# Title for the chart
-st.title("Bar Chart")
-
 # Setting the X and Y labels
 st.xlabel('Day')
 st.ylabel('Tip')
+st.pyplot()
+
+# Scatter plot using seaborn
+sns.scatterplot(x='day', y='tip', data=data, hue='sex')
+
+# Title for the plot
+plt.title("Scatter Plot")
+
+# Show plot in Streamlit app
 st.pyplot()
