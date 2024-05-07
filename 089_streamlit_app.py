0f1b11d584e561a21375d305e9f43574e1c00090
Khoirunisa Jannatuzzahra <105572998+KhoirunisaJ@users.noreply.github.com>
2024-05-07 12:18:23 +0700

    Update 089_streamlit_app.py

--- 089_streamlit_app.py
+++ 089_streamlit_app.py
@@ -1,19 +1,15 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
+import matplotlib.pyplot as plt
 
 
 # reading the database
-data = pd.read_csv("tips.csv")
+file_path = "tips.csv"
+data = pd.read_csv(file_path, encoding='latin-1')
 
 # Bar chart with day against tip
 plt.bar(data['day'], data['tip'])
-
 plt.title("Bar Chart")
-
 # Setting the X and Y labels
 plt.xlabel('Day')
 plt.ylabel('Tip')
-
-# Adding the legends
-plt.show()
