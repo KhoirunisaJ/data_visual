14bd18857d6e5f4747570e3d12dfa12b369d781a
Khoirunisa Jannatuzzahra <105572998+KhoirunisaJ@users.noreply.github.com>
2024-05-07 13:38:18 +0700

    Update 089_streamlit_app.py

--- 089_streamlit_app.py
+++ 089_streamlit_app.py
@@ -13,16 +13,12 @@ st.title("Bar Chart")
 # Bagian visualisasi data
 st.header("Selamat Datang")
 # Title for the chart
-st.subheader("Bar Chart")
+st.subheader("Bar Chart dan Scatter Plot")
 st.subheader("Jumlah Tips per hari")
 
-st.subheader("Scatter Plot")
-st.subheader("Jumlah Tips per hari")
 fig, ax = plt.subplots()
 sns.scatterplot(x='day', y='tip', data=data, hue='sex', ax=ax)
 
-# Bar chart with day against tip
 st.bar_chart(data.groupby('day')['tip'].mean())
 
-# Show plot in Streamlit app
 st.pyplot(fig)
